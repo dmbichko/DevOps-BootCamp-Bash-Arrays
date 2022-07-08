a5581ca252757ca5a82ded5bc6377a6169959c6b
dmbichko <101453211+dmbichko@users.noreply.github.com>
2022-07-08 07:27:48 +0300

    update

--- array.sh
+++ array.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
+count=0
+DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
+for i in {a..z} ; do
+        [[ $count -eq $1 ]] && break
+        mkdir $DIR"/folder_"$i
+        let "count+=1"
+done
 # Place your code here
