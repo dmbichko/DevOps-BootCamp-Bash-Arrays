0e08fc9f9e58f4e830df0f00beaf3246409cf352
Dzmitry Bichko <dzmitry_bichko@epam.com>
2022-07-08 19:07:20 +0300

    remove path file

--- array.sh
+++ array.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 count=0
-DIR="$( cd "$( dirname "$0" )" >/dev/null 2>&1 && pwd )"
 for i in {a..z} ; do
         [[ $count -eq $1 ]] && break
-        mkdir $DIR"/folder_"$i
+        mkdir "./folder_"$i
         let "count+=1"
 done
